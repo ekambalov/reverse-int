0c5ea7fb5f3e2db41004c4123f34f9e20437f78d
ekambalov <emakcomp@gmail.com>
2022-02-22 21:10:41 +0300

    feat: finish job

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(numb) {
+    numb = String(numb);
+    if (numb[0] == '-') numb = numb.slice(1);
+    let newNumb = '';
+    for (let i = 0; i < numb.length; i++) {
+        newNumb = newNumb + numb[numb.length - 1 - i];
+    }
+    newNumb = +newNumb;
+    return newNumb;
 }
